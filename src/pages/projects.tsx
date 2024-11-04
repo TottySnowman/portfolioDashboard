2a8a661245b0109108cb7d2a73e71c6c65b589e9
Paul-Philippe Wortmann <65237023+TottySnowman@users.noreply.github.com>
2024-11-04 07:19:18 +0100

    add project display and click events

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -2,6 +2,18 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../store/store";
 import { getProjects } from "../store/slices/projectSlice";
+import {
+  CircularProgress,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
 
 const Projects = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -13,15 +25,57 @@ const Projects = () => {
     dispatch(getProjects());
   }, [dispatch]);
 
-  if (loading) return <p>Loading...</p>;
+  const displayProject = (projectID: number) => {
+    console.log(projectID);
+  };
+
+  const editProject = (projectID: number) => {
+    console.log(projectID);
+  };
+
+  if (loading) return <CircularProgress />;
   if (error) return <p>Error: {error}</p>;
   return (
-    <div>
-      <p>Projects</p>
-      {projects.map((project) => (
-        <p>{project.Name}</p>
-      ))}
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Image</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Dev date</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Displayed</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {projects.map((project) => (
+            <TableRow
+              key={project.ProjectID}
+              hover
+              onClick={() => displayProject(project.ProjectID)}
+              style={{ cursor: "pointer" }}
+            >
+              <TableCell>{project.Name}</TableCell>
+              <TableCell>{project.Name}</TableCell>
+              <TableCell>{project.Name}</TableCell>
+              <TableCell>{project.Name}</TableCell>
+              <TableCell>{project.Name}</TableCell>
+              <TableCell>
+                <IconButton
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    editProject(project.ProjectID);
+                  }}
+                >
+                  <EditIcon/>
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
